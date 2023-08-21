e3ff054f0d516af756c41ce6de7e8ab54c638e86
mohamed37sad <simo.sad04@gmail.com>
2023-08-21 13:30:52 -0700

    updat

--- main.h
+++ main.h
@@ -3,12 +3,16 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <stdarg.h>
 
 int _printf(const char *format, ...);
 int _putchar(char c);
 int selfmadeputs(char *s);
-int str_len(char *s);
 void int_to_str(int num, char *buffer);
 void reverse(char *buffer, int length);
+int str_len(char *s);
+int handle_char(va_list arg);
+int handle_str(va_list arg);
+int handle_int(va_list arg);
 
 #endif
